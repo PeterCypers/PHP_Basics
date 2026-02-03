70dd8d0730edbd2508d221506c20d781f376835d
PeterCypers <peter-cypers@hotmail.com>
2026-02-03 14:27:40 +0100

    video #5 completed

--- pagepervideo/video_5.php
+++ pagepervideo/video_5.php
@@ -38,7 +38,36 @@
     echo "<br>";
     echo $_SERVER["REQUEST_METHOD"];
     echo "<br>-------------<br>";
-    echo "test change post re-install XAMPP"
+    echo "test change post re-install XAMPP";
+    echo "<br>";
+  ?>
+
+  <?php
+  // explaining GET
+  echo $_GET["name"];
+  // http://localhost/MyWebsite/pagepervideo/video_5.php?name=peter
+  /*
+  all the data in the URL is accessed as an 'associative array'
+  so, enter above url, then refresh and name is echo'd
+  */
+  // http://localhost/MyWebsite/pagepervideo/video_5.php?name=peter&eyecolor=blue
+  echo $_GET["eyecolor"];
+
+  // get vs post: with get the variables are in the URL, with post they aren't visible in the URL
+  // rule of thumbs: if you want to just get data: GET, if you want to add data: POST
+  // exception: login, we POST login data for secrecy so the login-email/name + password are not visible in the URL
+
+  // request works on GET/POST/COOKIES (but is less clear what type will return)
+  echo $_REQUEST["name"];
+
+  echo $_FILES["filename"];
+
+  echo $_COOKIE["cookiename"];
+
+  $_SESSION["username"] = "Peter";
+  echo $_SESSION["username"];
+
+  $_ENV["usersecret"]; // will talk about in a later episode
   ?>
   
 </body>
