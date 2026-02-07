4b29e5c7eee54b7d7216e50638acb97851ba83cd
PeterCypers <peter-cypers@hotmail.com>
2026-02-07 06:04:24 +0100

    video #9 completed

--- pagepervideo/video_9.php
+++ pagepervideo/video_9.php
@@ -5,13 +5,74 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Vid 9</title>
   <link rel="shortcut icon" href="./assets/new-php-logo.png" type="image/x-icon">
+  <link rel="stylesheet" href="../css/video_9.css">
 </head>
 <body>
   <a href="http://localhost/MyWebsite/">home</a>
   <?php echo "<h1>9. PHP Exercise for Beginners: Create a Calculator!</h1>" ?>  
+  <small>(note: darkmode styling made it difficult to see PHP-errors which show up as black text on the page)</small>
 
-  <?php
-    
-  ?>
+  <div>
+    <!-- server superglobal & data sanitization & the current page-->
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+      <input type="number" name="num01" placeholder="Number one">
+      <select name="operator">
+        <option value="add">+</option>
+        <option value="subtract">-</option>
+        <option value="multiply">*</option>
+        <option value="divide">/</option>
+      </select>
+      <input type="number" name="num02" placeholder="Number two">
+      <button>Calculate</button>
+    </form>
+
+    <?php
+    // always sanitize user input
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // $num01 = $_POST["num01"]; // not secure
+        $num01 = filter_input(INPUT_POST, "num01",
+        FILTER_SANITIZE_NUMBER_FLOAT);
+        $num02 = filter_input(INPUT_POST, "num02",
+        FILTER_SANITIZE_NUMBER_FLOAT);
+        $operator = htmlspecialchars($_POST["operator"]);
+
+        // Error handlers
+        $errors = false;
+
+        if (empty($num01) || empty($num02) || empty($operator)) {
+          echo "<p class='calc-error'>Fill in all fields!</p>";
+          $errors = true;
+        }
+
+        if (!is_numeric($num01) || !is_numeric($num02)) {
+          echo "<p class='calc-error'>Only write numbers!</p>";
+          $errors = true;
+        }
+
+        // Calculate the numbers if no errors
+        if (!$errors) {
+          $value = 0;
+          switch ($operator) {
+            case "add":
+              $value = $num01 + $num02;
+              break;
+            case "subtract":
+              $value = $num01 - $num02;
+              break;
+            case "multiply":
+              $value = $num01 * $num02;
+              break;
+            case "divide":
+              $value = $num01 / $num02;
+              break;
+            default:
+              echo "<p class='calc-error'>Something went horribly wrong!</p>";
+          }
+
+          echo "<p classs='calc-result'>Result = " . $value . "</p>";
+        }
+      }
+    ?>
+  </div>
 </body>
 </html>
